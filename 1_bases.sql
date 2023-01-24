2148e0614b58f0a705346073ef3c417723b66689
Christian Lisangola <jochri3@gmail.com>
2023-01-24 16:10:53 +0300

    2. Création d'une table avec contraintes

--- 1_bases.sql
+++ 1_bases.sql
@@ -1,6 +1,23 @@
+-- 1. Créer une table
 CREATE TABLE clients(
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   email VARCHAR(100),
   phone_number VARCHAR(15)
 );
+
+-- 2. Créer une table et appliquer des contraintes
+CREATE TABLE clients(
+--    nombre, qui s'inctement automatiquement, unique
+  id SERIAL PRIMARY KEY,
+    
+ -- NOT NULL : La valeur doit toujours être renseignée
+ --  UNIQUE : Appliquer une constrainte d'unicité   
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  phone_number VARCHAR(15) UNIQUE
+);
+
+-- 3. Supprimer une table
+-- DROP TABLE clients;
